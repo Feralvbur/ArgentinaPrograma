4de11c9680324977faaabef1dd84947dca184b6b
Fernando <fer>
2023-05-20 12:47:28 -0300

    Practico N0 finalizado

---
+++ Practico0/Promedio.js
@@ -0,0 +1,17 @@
+let x = 5;
+let y = 7;
+let z = 9;
+
+console.log("El Promedio de x+y+z = "+(x+y+z)/3)
+
+let x1 = -2;
+let y1 = -3;
+let z1 = -8;
+
+console.log("El Promedio de x+y+z = "+ (x1+y1+z1)/3)
+
+let x2 = -1.2;
+let y2 = 5.3;
+let z2 = -3.3;
+
+console.log ("El promedio de x+y+z = "+ (x2+y2+z2)/3)
---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 3;
+
+console.log("Area = "+ (base*altura));
+let base2 = 12.5;
+let altura2 = 5.3;
+
+console.log("Area = "+(base2*altura2))
